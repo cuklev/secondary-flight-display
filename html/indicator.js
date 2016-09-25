0303f164d7b866b007e9c8526f8f338ece62b130
Kristiyan Tsaklev <cuki@cukii.tk>
2016-09-25 19:16:26 +0300

    Add reference point

--- html/indicator.js
+++ html/indicator.js
@@ -29,31 +29,43 @@ let indicator = (function() {
 
 		ctx.translate(canvas.width / 2, canvas.height / 2);
 
-		ctx.translate(0, pitch * canvas.height / Math.PI);
 		ctx.rotate(-roll);
+		ctx.translate(0, pitch * canvas.height / Math.PI);
 
+		// horizon
 		ctx.fillStyle = '#004AFF'; // blue
 		ctx.fillRect(-canvas.width, -canvas.height, canvas.width * 2, canvas.height);
 		ctx.fillStyle = '#523108'; // brown
 		ctx.fillRect(-canvas.width, 0, canvas.width * 2, canvas.height);
 
+		// scale
+		ctx.strokeStyle = 'yellow';
+		ctx.lineWidth = 1;
+		ctx.beginPath();
+		for(let y = -90; y <= 90; y += 10) {
+			ctx.moveTo(-40, y * canvas.height / 180);
+			ctx.lineTo(40, y * canvas.height / 180);
+		}
+		for(let y = -25; y <= 25; y += 10) {
+			ctx.moveTo(-15, y * canvas.height / 180);
+			ctx.lineTo(15, y * canvas.height / 180);
+		}
+		ctx.stroke();
+
 		ctx.resetTransform();
 
 		ctx.translate(canvas.width / 2, canvas.height / 2);
 
-		ctx.strokeStyle = 'yellow';
-		for(let y = -90; y <= 90; y += 10) {
-			ctx.beginPath();
-			ctx.moveTo(-40, y * canvas.height / 180);
-			ctx.lineTo(40, y * canvas.height / 180);
-			ctx.stroke();
-		}
-		for(let y = -25; y <= 25; y += 10) {
-			ctx.beginPath();
-			ctx.moveTo(-15, y * canvas.height / 180);
-			ctx.lineTo(15, y * canvas.height / 180);
-			ctx.stroke();
-		}
+		// plane
+		ctx.strokeStyle = 'red';
+		ctx.lineWidth = 3;
+		ctx.beginPath();
+		ctx.moveTo(-canvas.width / 2, 0);
+		ctx.lineTo(-20, 0);
+		ctx.moveTo(canvas.width / 2, 0);
+		ctx.lineTo(20, 0);
+		ctx.arc(0, 0, 20, 0, Math.PI);
+		ctx.stroke();
 
 		ctx.resetTransform();
 
