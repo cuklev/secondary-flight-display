a3c3b229f629495ae6328e4e30645c63491f2f4e
Kristiyan Tsaklev <cuki@cukii.tk>
2016-09-25 18:55:05 +0300

    Add pitch scale

--- html/indicator.js
+++ html/indicator.js
@@ -28,6 +28,7 @@ let indicator = (function() {
 		ctx.clearRect(0, 0, canvas.width, canvas.height);
 
 		ctx.translate(canvas.width / 2, canvas.height / 2);
+
 		ctx.translate(0, pitch * canvas.height / Math.PI);
 		ctx.rotate(-roll);
 
@@ -35,6 +36,24 @@ let indicator = (function() {
 		ctx.fillRect(-canvas.width, -canvas.height, canvas.width * 2, canvas.height);
 		ctx.fillStyle = '#523108'; // brown
 		ctx.fillRect(-canvas.width, 0, canvas.width * 2, canvas.height);
+
+		ctx.resetTransform();
+
+		ctx.translate(canvas.width / 2, canvas.height / 2);
+
+		ctx.strokeStyle = 'yellow';
+		for(let y = -90; y <= 90; y += 10) {
+			ctx.beginPath();
+			ctx.moveTo(-40, y * canvas.height / 180);
+			ctx.lineTo(40, y * canvas.height / 180);
+			ctx.stroke();
+		}
+		for(let y = -25; y <= 25; y += 10) {
+			ctx.beginPath();
+			ctx.moveTo(-15, y * canvas.height / 180);
+			ctx.lineTo(15, y * canvas.height / 180);
+			ctx.stroke();
+		}
 
 		ctx.resetTransform();
 
